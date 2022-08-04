27f81a042754e3e146209d3d776abaaec9f1db10
Moisés Osg <71807776+AOV2099@users.noreply.github.com>
2022-08-04 07:34:34 -0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
 
     public static void main(String[] args) {
     
-        System.out.println("Hello Changes! 1 ");
+        System.out.println("Hello Changes! 2 ");
     }
 }
